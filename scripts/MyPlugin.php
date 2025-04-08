d9a39ed5c15e25649204fd778ceeb598a2e02532
Bhavarth Karmarakr <bhavarth1905kr@gmail.com>
2025-04-08 09:35:23 +0530

    fixed MyPlugin.php

--- scripts/MyPlugin.php
+++ scripts/MyPlugin.php
@@ -6,6 +6,7 @@ use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PluginEvents;
 use Composer\Script\Event;
 
 class MyPlugin implements PluginInterface, EventSubscriberInterface
@@ -13,6 +14,16 @@ class MyPlugin implements PluginInterface, EventSubscriberInterface
     public function activate(Composer $composer, IOInterface $io)
     {
         // You can store references if needed
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // Optional: Cleanup when plugin is deactivated
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // Optional: Cleanup when plugin is uninstalled
     }
 
     public static function getSubscribedEvents()
